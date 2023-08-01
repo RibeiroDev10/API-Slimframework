6e6e64019a5853e85a4ca4d45acee335f7117661
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-08-01 20:26:09 -0300

    sql att

--- App/Controllers/ProductController.php
+++ App/Controllers/ProductController.php
@@ -5,11 +5,15 @@ namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-final class ProductController{
+final class ProductController
+{
 
-    public function getProducts(Request $request, Response $response, array $args){
+    public function getProducts(Request $request, Response $response, array $args)
+    {
 
-        $response->getBody()->write("Hello World!");
+        $response = $response->withJSON([
+            "message" => "Hello World!"
+        ]);
 
         return $response;
     }
