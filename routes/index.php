5cf5601c80a15fa9e4dc88e762229e8c94ad0e40
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-08-01 22:34:58 -0300

    updatesssssss

--- routes/index.php
+++ routes/index.php
@@ -1,12 +1,24 @@
 <?php
 
 use src\slimConfiguration;
-use App\Controllers\ProductController;
+use App\Controllers\ProdutoController;
+use App\Controllers\LojaController;
 
 $app = new \Slim\App();
+
 // ==================================================================
 
-$app->get('/', ProductController::class . ':getProducts');
+// LOJAS
+$app->get('/loja', LojaController::class . ':getLojas');
+$app->post('/loja', LojaController::class . ':insertLojas');
+$app->put('/loja', LojaController::class . ':updateLojas');
+$app->delete('/loja', LojaController::class . ':deleteLojas');
+
+// PRODUTOS
+$app->get('/produto', ProdutoController::class . ':getProdutos');
+$app->post('/produto', ProdutoController::class . ':insertProdutos');
+$app->put('/produto', ProdutoController::class . ':updateProdutos');
+$app->delete('/produto', ProdutoController::class . ':deleteProdutos');
 
 // ==================================================================
 $app->run();
